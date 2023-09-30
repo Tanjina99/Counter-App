31d57420181c81a4d5a7763efa0da473811a6d13
tanjina sultana <tanjina1997@yahoo.com>
2023-09-29 21:58:37 -0400

    added the js file

---
+++ script.js
@@ -0,0 +1,32 @@
+
+const count = document.getElementById('count');
+const incrementButton = document.getElementById('increment');
+const decrementButton = document.getElementById('decrement');
+const resetButton = document.getElementById('reset');
+
+
+let counter = 0;
+
+function updateCounter() {
+  countElement.textContent = counter;
+}
+
+incrementButton.addEventListener('click', () => {
+    counter++;
+    updateCounter();
+  });
+  
+
+decrementButton.addEventListener('click', () => {
+  counter--;
+  updateCounter();
+});
+
+
+resetButton.addEventListener('click', () => {
+  counter = 0;
+  updateCounter();
+});
+
+
+updateCounter();
